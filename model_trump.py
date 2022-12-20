f28a5d49eb2d2dfa2554b87c8487c3d4e7d63466
ChrisDre <drechselchris@gmail.com>
2022-12-20 15:37:18 +0100

    Preprocessing improved. Implementing first algos.

--- model_trump.py
+++ model_trump.py
@@ -4,12 +4,12 @@ import seaborn as sns
 import numpy as np
 from pandas.api.types import CategoricalDtype
 import matplotlib.pyplot as plt
+from pandas.plotting import scatter_matrix
 
 
-
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, OrdinalEncoder, OneHotEncoder
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error, r2_score
@@ -18,10 +18,21 @@ from sklearn.decomposition import PCA
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.feature_selection import mutual_info_regression
 from sklearn.impute import SimpleImputer
-from sklearn.decomposition import FactorAnalysis 
+from sklearn.decomposition import FactorAnalysis
 from sklearn.ensemble import ExtraTreesClassifier
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
 
+
+# metrics
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 
 
 # load variable list from excel sheet
@@ -32,7 +43,7 @@ chosen_vars = df_vars["Variable"].dropna().unique().tolist()
 
 # load gss data from 2018
 df = pd.read_pickle("gss_2018_no_labels.pkl")
-#df = pd.read_pickle("gss_2018_labels.pkl")
+# df = pd.read_pickle("gss_2018_labels.pkl")
 
 # filter for mental vars
 df = df[chosen_vars]
@@ -46,110 +57,114 @@ ind_codes = dict(zip(ind_codes_xl["subsector"].to_list(), ind_codes_xl["id"].to_
 # preprocessing with labels
 def preprocessing(df):
 
-    # drop "year" column
+    # year: -> drop "year" column
     df.drop("year", axis=1, inplace=True)
 
-    # remove rows with missing vote in 2016 (1 = Clinton, 2 = Trump, 3 = other candidate)
+    # id: -> do nothing
+
+    # pres16: (Target Variable, 2016 presidential vote) -> remove rows with missing vote in 2016 (1 = Clinton, 2 = Trump, 3 = other candidate)
     df = df[df.pres16.isin([1, 2, 3])]
 
-    # pres16: transform to trump var
-    df['trump'] = np.where(df['pres16'] == 2, 1, 0)
+    # pres16: transform to binary
+    df["trump"] = np.where(df["pres16"] == 2, 1, 0)
     df.drop("pres16", axis=1, inplace=True)
 
+    # wrkstat: (Work Status) -> nominal categorical
+    df["wrkstat"] = df["wrkstat"].astype("category")
 
-    # replace missing values in hrs1 with hrs2 or zero for retired/unemp etc.
-    df["hours_worked"] = df['hrs1'].where(~df["wrkstat"].isin([4, 5, 6, 7, 8]), 0)
+    # hrs1: (Hours worked last week) -> replace missing values in hrs1 with hrs2 or zero for retired/unemp etc.
+    df["hours_worked"] = df["hrs1"].where(~df["wrkstat"].isin([4, 5, 6, 7, 8]), 0)
     df["hours_worked"] = np.where(df["wrkstat"] == 3, df["hrs2"], df["hours_worked"])
     df.dropna(subset=["hours_worked"], inplace=True)
     df.drop("hrs1", axis=1, inplace=True)
     df.drop("hrs2", axis=1, inplace=True)
 
-    # wrkstat
-    df["wrkstat"] = df["wrkstat"].astype("category")
-
-    # wrkslf: if missing but retired/unemp etc. set to not self employed
+    # wrkslf: (Self employed or not) -> if missing but retired/unemp etc. set to not self employed
     df["selfemp"] = df["wrkslf"].map({1: 1, 2: 0})
     df["selfemp"] = df["selfemp"].fillna(0)
     df.drop("wrkslf", axis=1, inplace=True)
 
-    # fill missing "age" cells with mean (4 missing)
+    # age: -> fill missing "age" cells with mean (4 missing)
     df["age"].fillna(df["age"].mean(), inplace=True)
 
-    # "sex" column -> transform to "female" column
+    # sex: (gender) -> transform to binary "female" column
     df["female"] = df["sex"].map({2: 1, 1: 0})
     df.drop("sex", axis=1, inplace=True)
 
-    # satjob: new category: 0 for retired/unemp etc. or no answer
-    df["satjob"].fillna(0, inplace=True)
-    df["satjob"] = df["satjob"].astype("category")
+    # educ: (Education in years) -> do nothing
 
-    # wrktype: new category: 0 for retired/unemp etc. or no answer
-    df["wrktype"].fillna(0, inplace=True)
+    # satjob: (job satisfaction) -> ordinal categorical
+    # original order is 1 - Very satisfied to 4 - very dissatisfied -> reverse, because higher number means better
+    satjob_mapper = {1: 4, 2: 3, 3: 2, 4: 1}
+    df["jobsat"] = df["satjob"].replace(satjob_mapper)
+    df.drop("satjob", axis=1, inplace=True)
+
+    # wrktype: (type of work) -> nominal categorical
     df["wrktype"] = df["wrktype"].astype("category")
 
-    # drop na rows
-    df.dropna(inplace=True)
-
-    # marital
-    df["marital"] = df["marital"].astype("category")
-
-    # partyid
-    df["partyid"] = df["partyid"].astype("category")
-
-    # relig: nothing
+    # relig: (Religion) -> nominal categorical
     df["relig"] = df["relig"].astype("category")
 
-    # income16
-    df["income16"] = df["income16"].astype("category")
-
-    # wrkgovt, transform, remove missing later
+    # wrkgovt: (Gov employee or not) -> transform to binary govemp column
     df["govemp"] = df["wrkgovt"].map({1: 1, 2: 0})
     df.drop("wrkgovt", axis=1, inplace=True)
 
+    # prestg10: (prestige score of occupation) -> do nothing
 
-    for col in ["natsoc", "natmass", "natpark", "natchld", "natsci", "natenrgy"]:
-        df[col] = df[col].astype("category")
-
-
-    # indus10
+    # indus10: (Industry of work) -> nominal, high cardinality, collapse to fewer with industry codes
+    # reduces industries from 232 to 12
     df["industry"] = df["indus10"].map(ind_codes)
     df["industry"] = df["industry"].astype("category")
     df.drop("indus10", axis=1, inplace=True)
 
-    # hispanic: 1 = not hispanic, else hispanic
-    df['hispanic'] = np.where(df['hispanic'] == 1, 0, 1)
+    # marital: (marriage status) -> nominal categorical
+    df["marital"] = df["marital"].astype("category")
 
-    # capppun -> transform to favor death penalty
+    # partyid: (political party affiliation) -> nominal categorical
+    df["partyid"] = df["partyid"].astype("category")
+
+    # vote12: (if voted in 2012) -> transform to binary voted_12 column
+    df["voted_12"] = np.where(df["vote12"] == 1, 1, 0)
+    df.drop("vote12", axis=1, inplace=True)
+
+    # vote16: (same as vote12)
+    df["voted_16"] = np.where(df["vote16"] == 1, 1, 0)
+    df.drop("vote16", axis=1, inplace=True)
+
+    # income16: (income category) -> ordinal categorical
+    # TODO: check value counts and maybe collapse to fewer categories
+
+    # nat variables: (opinion, if spending by govt is too little, about right or too much) -> nominal categorical
+    for col in ["natsoc", "natmass", "natpark", "natchld", "natsci", "natenrgy"]:
+        df[col] = df[col].astype("category")
+
+    # capppun: (favor or oppose death pen) -> transform to binary fav_death_pen column
     df["fav_death_pen"] = df["cappun"].map({1: 1, 2: 0})
     df.drop("cappun", axis=1, inplace=True)
 
-    # vote12
-    df['voted_12'] = np.where(df['vote12'] == 1, 1, 0)
-    df.drop("vote12", axis=1, inplace=True)
+    # hispanic: (whether or not is hispanic) -> transform to binary column
+    df["hispanic"] = np.where(df["hispanic"] == 1, 0, 1)
 
-    # vote16
-    df['voted_16'] = np.where(df['vote16'] == 1, 1, 0)
-    df.drop("vote16", axis=1, inplace=True)
+    # vetyears: (years in armed forces) -> do nothing
 
-    # TODO: transform categories to categorical
-    # -> ordinal: consecutive integers (higher is better)
-    # -> nominal: OHE
+    # drop na rows
+    df.dropna(inplace=True)
 
     return df
-
-
-    # wrktype
-
-
 
 
 # analytical base table (abt)
 abt = df.copy()
 
+# run dataframe through preprocessing
 abt = preprocessing(abt)
 
+# show info
+print(abt.info())
 
-abt.info()
+# TODO: visualization and summarization
+# TODO: feature selection
+# TODO: find classification algos
 
 ## Models
 
@@ -159,13 +174,68 @@ y = abt["trump"]
 # create feature colummns
 X = abt.drop(["trump", "id"], axis=1)
 
-# OHE
+# One Hot Encoding (create dummy vars for nominal categories), set drop_first=True to avoid dummy variable trap
 X = pd.get_dummies(X, drop_first=True)
 
-
+# create train and test splits
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=1111
 )
+
+
+# TODO: it is now a classifier problem
+np.random.seed(5000)
+
+# init list for comparison
+models = []
+
+
+def algo_models(X_train, X_test, y_train, y_test):
+
+    models.append(("LR", LogisticRegression()))
+    models.append(("CART", DecisionTreeClassifier()))
+    models.append(("KNN", KNeighborsClassifier()))
+    models.append(("NB", GaussianNB()))
+    models.append(("LDA", LinearDiscriminantAnalysis()))
+    models.append(("SVM", SVC()))
+
+    outcomes = []
+    names = []
+
+    # loop through models
+    for name, model in models:
+        v_results = cross_val_score(
+            model, X_train, y_train, cv=3, scoring="accuracy", n_jobs=-1, verbose=0
+        )
+        print(name, v_results.mean())
+        outcomes.append(v_results)
+        names.append(name)
+
+    print(outcomes)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_xticklabels(names)
+    plt.boxplot(outcomes)
+
+
+algo_models(X_train, X_test, y_train, y_test)
+
+
+# Evaluating and predicting models
+for name, model in models:
+    trainedmodel = model.fit(X_train, y_train)
+
+    # prediction
+    ypredict = trainedmodel.predict(X_test)
+
+    acc = accuracy_score(y_test, ypredict)
+    classreport = classification_report(y_test, ypredict)
+    confMat = confusion_matrix(y_test, ypredict)
+
+    print("\n****************************" + name)
+    print("The accuracy: {}".format(acc))
+    print("The Classification Report:\n {}".format(classreport))
+    print("The Confusion Matrix:\n {}".format(confMat))
 
 
 # mental
@@ -181,9 +251,6 @@ print("Mental: Coefficients: \n", logisticRegr.coef_)
 print("Mental: Mean squared error: %.2f" % mean_squared_error(y_test, y_pred))
 # The coefficient of determination: 1 is perfect prediction
 print("Mental: Coefficient of determination: %.2f" % r2_score(y_test, y_pred))
-
-
-
 
 
 # trump
@@ -262,16 +329,10 @@ print("DT: Mean squared error: %.2f" % mean_squared_error(y_test, y_pred))
 print("DT: Coefficient of determination: %.2f" % r2_score(y_test, y_pred))
 
 
-
 # k nearest neighbor
 neigh = KNeighborsRegressor(n_neighbors=2)
 neigh.fit(X_train, y_train)
 y_pred = neigh.predict(X_test)
-
-
-
-
-
 
 
 ## ML SPEED RUN
@@ -279,7 +340,8 @@ y_pred = neigh.predict(X_test)
 # setup ML pipelines
 pipelines = {
     "rf": make_pipeline(StandardScaler(), RandomForestRegressor(random_state=1234)),
-    "gb": make_pipeline(StandardScaler(), GradientBoostingRegressor(random_state=1234))}
+    "gb": make_pipeline(StandardScaler(), GradientBoostingRegressor(random_state=1234)),
+}
 
 
 grid = {
@@ -312,4 +374,4 @@ for algo, model in fit_models.items():
     print(f"Metrics for {algo}: MSE:{mse}, R2: {r2}, MAP: {map}")
 
 
-x= "stop"
+x = "stop"

fc7df79746d4cb42ebdb52377cd37d32bd4f21ee
ChrisDre <drechselchris@gmail.com>
2022-12-25 17:16:21 +0100

    Adding hyperparameter tuning

--- model_trump.py
+++ model_trump.py
@@ -12,17 +12,18 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler, OrdinalEncoder, OneHotEncoder
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error, r2_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn import linear_model, tree
 from sklearn.decomposition import PCA
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.feature_selection import mutual_info_regression
 from sklearn.impute import SimpleImputer
 from sklearn.decomposition import FactorAnalysis
-from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn.exceptions import NotFittedError
 
 
-from sklearn.linear_model import LogisticRegression, RidgeClassifier, RidgeClassifierCV
+from sklearn.linear_model import LogisticRegression, RidgeClassifierCV
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
@@ -56,102 +57,105 @@ ind_codes = dict(zip(ind_codes_xl["subsector"].to_list(), ind_codes_xl["id"].to_
 
 
 # preprocessing with labels
-def preprocessing(df):
+def preprocessing(raw_df):
+
+    # reset index
+    raw_df.reset_index(drop=True, inplace=True)
 
     # year: -> drop "year" column
-    df.drop("year", axis=1, inplace=True)
+    raw_df.drop("year", axis=1, inplace=True)
 
     # id: -> do nothing
 
     # pres16: (Target Variable, 2016 presidential vote) -> remove rows with missing vote in 2016 (1 = Clinton, 2 = Trump, 3 = other candidate)
-    df = df[df.pres16.isin([1, 2, 3])]
+    raw_df = raw_df[raw_df.pres16.isin([1, 2, 3])].copy()
 
     # pres16: transform to binary
-    df["trump"] = np.where(df["pres16"] == 2, 1, 0)
-    df.drop("pres16", axis=1, inplace=True)
+    raw_df["trump"] = np.where(raw_df["pres16"] == 2, 1, 0)
+    raw_df.drop("pres16", axis=1, inplace=True)
 
     # wrkstat: (Work Status) -> nominal categorical
-    df["wrkstat"] = df["wrkstat"].astype("category")
+    raw_df["wrkstat"] = raw_df["wrkstat"].astype("category")
 
     # hrs1: (Hours worked last week) -> replace missing values in hrs1 with hrs2 or zero for retired/unemp etc.
-    df["hours_worked"] = df["hrs1"].where(~df["wrkstat"].isin([4, 5, 6, 7, 8]), 0)
-    df["hours_worked"] = np.where(df["wrkstat"] == 3, df["hrs2"], df["hours_worked"])
-    df.dropna(subset=["hours_worked"], inplace=True)
-    df.drop("hrs1", axis=1, inplace=True)
-    df.drop("hrs2", axis=1, inplace=True)
+    raw_df["hours_worked"] = raw_df["hrs1"].where(~raw_df["wrkstat"].isin([4, 5, 6, 7, 8]), 0)
+    raw_df["hours_worked"] = np.where(raw_df["wrkstat"] == 3, raw_df["hrs2"], raw_df["hours_worked"])
+    raw_df.dropna(subset=["hours_worked"], inplace=True)
+    raw_df.drop("hrs1", axis=1, inplace=True)
+    raw_df.drop("hrs2", axis=1, inplace=True)
 
     # wrkslf: (Self employed or not) -> if missing but retired/unemp etc. set to not self employed
-    df["selfemp"] = df["wrkslf"].map({1: 1, 2: 0})
-    df["selfemp"] = df["selfemp"].fillna(0)
-    df.drop("wrkslf", axis=1, inplace=True)
+    raw_df["selfemp"] = raw_df["wrkslf"].map({1: 1, 2: 0})
+    raw_df["selfemp"] = raw_df["selfemp"].fillna(0)
+    raw_df["selfemp"] = raw_df["selfemp"].astype('int')
+
+
+    raw_df.drop("wrkslf", axis=1, inplace=True)
 
     # age: -> fill missing "age" cells with mean (4 missing)
-    df["age"].fillna(df["age"].mean(), inplace=True)
+    raw_df["age"].fillna(raw_df["age"].mean(), inplace=True)
 
     # sex: (gender) -> transform to binary "female" column
-    df["female"] = df["sex"].map({2: 1, 1: 0})
-    df.drop("sex", axis=1, inplace=True)
+    raw_df["female"] = raw_df["sex"].map({2: 1, 1: 0})
+    raw_df.drop("sex", axis=1, inplace=True)
 
     # educ: (Education in years) -> do nothing
 
-    # satjob: (job satisfaction) -> ordinal categorical
-    # original order is 1 - Very satisfied to 4 - very dissatisfied -> reverse, because higher number means better
-    satjob_mapper = {1: 4, 2: 3, 3: 2, 4: 1}
-    df["jobsat"] = df["satjob"].replace(satjob_mapper)
-    df.drop("satjob", axis=1, inplace=True)
-
-    # wrktype: (type of work) -> nominal categorical
-    df["wrktype"] = df["wrktype"].astype("category")
-
     # relig: (Religion) -> nominal categorical
-    df["relig"] = df["relig"].astype("category")
+    raw_df["relig"] = raw_df["relig"].astype("category")
+
 
     # wrkgovt: (Gov employee or not) -> transform to binary govemp column
-    df["govemp"] = df["wrkgovt"].map({1: 1, 2: 0})
-    df.drop("wrkgovt", axis=1, inplace=True)
+    raw_df["govemp"] = raw_df["wrkgovt"].map({1: 1, 2: 0})
+    raw_df.drop("wrkgovt", axis=1, inplace=True)
 
     # prestg10: (prestige score of occupation) -> do nothing
 
     # indus10: (Industry of work) -> nominal, high cardinality, collapse to fewer with industry codes
     # reduces industries from 232 to 12
-    df["industry"] = df["indus10"].map(ind_codes)
-    df["industry"] = df["industry"].astype("category")
-    df.drop("indus10", axis=1, inplace=True)
+    raw_df["industry"] = raw_df["indus10"].map(ind_codes)
+    raw_df["industry"] = raw_df["industry"].astype("category")
+    raw_df.drop("indus10", axis=1, inplace=True)
 
     # marital: (marriage status) -> nominal categorical
-    df["marital"] = df["marital"].astype("category")
+    raw_df["marital"] = raw_df["marital"].astype("category")
 
     # partyid: (political party affiliation) -> nominal categorical
-    df["partyid"] = df["partyid"].astype("category")
+    raw_df["partyid"] = raw_df["partyid"].astype("category")
 
     # vote12: (if voted in 2012) -> transform to binary voted_12 column
-    df["voted_12"] = np.where(df["vote12"] == 1, 1, 0)
-    df.drop("vote12", axis=1, inplace=True)
+    raw_df["voted_12"] = np.where(raw_df["vote12"] == 1, 1, 0)
+    raw_df.drop("vote12", axis=1, inplace=True)
 
     # vote16: (same as vote12)
-    df["voted_16"] = np.where(df["vote16"] == 1, 1, 0)
-    df.drop("vote16", axis=1, inplace=True)
+    raw_df["voted_16"] = np.where(raw_df["vote16"] == 1, 1, 0)
+    raw_df.drop("vote16", axis=1, inplace=True)
 
     # income16: (income category) -> ordinal categorical
     # TODO: check value counts and maybe collapse to fewer categories
 
     # nat variables: (opinion, if spending by govt is too little, about right or too much) -> nominal categorical
     for col in ["natsoc", "natmass", "natpark", "natchld", "natsci", "natenrgy"]:
-        df[col] = df[col].astype("category")
+        raw_df[col] = raw_df[col].astype("category")
 
     # capppun: (favor or oppose death pen) -> transform to binary fav_death_pen column
-    df["fav_death_pen"] = df["cappun"].map({1: 1, 2: 0})
-    df.drop("cappun", axis=1, inplace=True)
+    raw_df["fav_death_pen"] = raw_df["cappun"].map({1: 1, 2: 0})
+    raw_df.drop("cappun", axis=1, inplace=True)
 
     # hispanic: (whether or not is hispanic) -> transform to binary column
-    df["hispanic"] = np.where(df["hispanic"] == 1, 0, 1)
+    raw_df["hispanic"] = np.where(raw_df["hispanic"] == 1, 0, 1)
 
     # vetyears: (years in armed forces) -> do nothing
 
     # drop na rows
-    df.dropna(inplace=True)
+    raw_df.dropna(inplace=True)
 
-    return df
+    # convert some misspecified columns to int
+    raw_df["govemp"] = raw_df["govemp"].astype('int')
+    raw_df["fav_death_pen"] = raw_df["fav_death_pen"].astype('int')
+
+
+    return raw_df
 
 
 # analytical base table (abt)
@@ -161,11 +165,14 @@ abt = df.copy()
 abt = preprocessing(abt)
 
 # show info
-print(abt.info())
+abt.info()
 
 # TODO: visualization and summarization
 # TODO: feature selection
 # TODO: find classification algos
+# Models: Logitstic Regression, Naive Bayes Classifier, K-Nearest Neighbor, Decision Tree, Random Forest, Artificial Neural Networks, Support Vector Machines:
+# Evaluation: CV, Classification Report, ROC Curve, 
+
 
 ## Models
 
@@ -178,6 +185,15 @@ X = abt.drop(["trump", "id"], axis=1)
 # One Hot Encoding (create dummy vars for nominal categories), set drop_first=True to avoid dummy variable trap
 X = pd.get_dummies(X, drop_first=True)
 
+# remove relig_8 to have same df as in R, (relig_8 does not exist in our processed data anyway)
+X.drop("relig_8.0", axis=1, inplace=True)
+
+
+# TODO: Scale continuous variables
+cont_vars = abt.select_dtypes(include=['float']).columns
+X[cont_vars] = StandardScaler().fit_transform(X[cont_vars])
+
+
 # create train and test splits
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=1111
@@ -188,28 +204,100 @@ X_train, X_test, y_train, y_test = train_test_split(
 np.random.seed(5000)
 
 
-# Ridge
-clf_rc = RidgeClassifier()
-clf_rc.fit(X_train, y_train)
+## WITH HYPERPARAMETER TUNING
+# make pipelines
+pipelines = {
+    "LR": make_pipeline(LogisticRegression(solver="saga", max_iter=10000, random_state=1234)),
+    "LR_enet": make_pipeline(LogisticRegression(penalty="elasticnet", solver="saga", max_iter=10000, random_state=1234)),
+    "Ridge": make_pipeline(RidgeClassifierCV()),
+    "DTC": make_pipeline(DecisionTreeClassifier(random_state=1234)),
+    "RFC": make_pipeline(RandomForestClassifier()),
+    "KN": make_pipeline(KNeighborsClassifier()),
+    "GNB": make_pipeline(GaussianNB()),
+    "LDA": make_pipeline(LinearDiscriminantAnalysis()),
+    "SVC": make_pipeline(SVC(random_state=1234)),
+    "GB": make_pipeline(GradientBoostingClassifier(random_state=1234))
+}
 
-clf_rc.score(X_train, y_train)
-clf_rc.score(X_test, y_test)
+# make hypergrid to find best parameters for each model
+hypergrid = {
+    "LR": {
+        "logisticregression__penalty": ["none", "l1", "l2"]
+        },
+    "LR_enet": {
+        "logisticregression__l1_ratio": [0.1, 0.5, 0.9]
+        },
+    "Ridge": {
+        "ridgeclassifiercv__alphas": [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 0.99]
+        },
+    "DTC": {
+        "decisiontreeclassifier__min_samples_split": [2, 4, 6],
+        "decisiontreeclassifier__min_samples_leaf": [1, 2, 3]
+        },
+    "RFC": {
+        "randomforestclassifier__n_estimators": [50, 100, 200],
+        },
+    "KN": {
+        "kneighborsclassifier__leaf_size": [10, 20, 30, 40, 50],
+        "kneighborsclassifier__n_neighbors": [2, 3, 5, 7, 10],
+        "kneighborsclassifier__p": [1, 2],
+        },
+    "GNB": {
+        "gaussiannb__var_smoothing": [1e-11, 1e-10, 1e-09]
+        },
+    "LDA": {
+        },
+    "SVC": {"svc__C": [0.1,1, 10, 100],
+            "svc__gamma": [1,0.1,0.01,0.001],
+            "svc__kernel": ['rbf', 'poly', 'sigmoid']
+        },
+    "GB": {"gradientboostingclassifier__n_estimators": [100, 200, 300]
+
+    }
+}
 
 
-clf_rcv = RidgeClassifierCV()
-clf_rcv.fit(X_train, y_train)
-
-clf_rcv.score(X_train, y_train)
-clf_rcv.score(X_test, y_test)
 
 
+fit_models = {}
+
+for algo, pipeline in pipelines.items():
+    model = GridSearchCV(pipeline, hypergrid[algo], cv=10, n_jobs=-1)
+    print("Training models...")
+    try:
+        model.fit(X_train, y_train)
+        fit_models[algo] = model
+        print(f"{algo} successful...")
+    except NotFittedError as e:
+        print(repr(e))
+    print("Training models done.")
+
+
+
+
+# evaluate performance on test partition
+for algo, model in fit_models.items():
+    print(f"Predicting {algo}...")
+    y_hat = model.predict(X_test)
+    print(f"Accuracy Score: {accuracy_score(y_test, y_hat)}")
+    print(f"Precision Score: {precision_score(y_test, y_hat)}")
+    print(f"Recall Score: {recall_score(y_test, y_hat)}")
+    
+
+
+
+
+
+
+
+## STANDARD MODEL WITHOUT HYPERPARAMETER TUNING
 # init list for comparison
 models = []
 
 
 def algo_models(X_train, X_test, y_train, y_test):
 
-    models.append(("LR", LogisticRegression(solver="saga", max_iter=10000)))
+    models.append(("LR", LogisticRegression(penalty='none', solver="saga", max_iter=10000)))
     models.append(
         ("LR_L1", LogisticRegression(penalty="l1", solver="saga", max_iter=10000))
     )
@@ -235,15 +323,16 @@ def algo_models(X_train, X_test, y_train, y_test):
     names = []
 
     # loop through models
+    print(f"********** CV **********")
     for name, model in models:
+        # 5-Fold Cross Validation (train, validate)
         v_results = cross_val_score(
-            model, X_train, y_train, cv=3, scoring="accuracy", n_jobs=-1, verbose=0
+            model, X_train, y_train, cv=5, scoring="accuracy", n_jobs=-1, verbose=0
         )
-        print(name, v_results.mean())
+        print(f"5-CV Accuracy for {name}: {v_results.mean()}")
         outcomes.append(v_results)
         names.append(name)
 
-    print(outcomes)
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.set_xticklabels(names)
@@ -255,16 +344,25 @@ algo_models(X_train, X_test, y_train, y_test)
 
 # Evaluating and predicting models
 for name, model in models:
-    trainedmodel = model.fit(X_train, y_train)
+    trained_model = model.fit(X_train, y_train)
 
     # prediction
-    ypredict = trainedmodel.predict(X_test)
+    y_pred = trained_model.predict(X_test)
 
-    acc = accuracy_score(y_test, ypredict)
-    classreport = classification_report(y_test, ypredict)
-    confMat = confusion_matrix(y_test, ypredict)
+    acc = accuracy_score(y_test, y_pred)
+    classreport = classification_report(y_test, y_pred)
+    conf_mat = confusion_matrix(y_test, y_pred)
+    coeffs = pd.DataFrame(zip(X.columns, trained_model.coef_[0]))
 
-    print("\n****************************" + name)
-    print("The accuracy: {}".format(acc))
-    print("The Classification Report:\n {}".format(classreport))
-    print("The Confusion Matrix:\n {}".format(confMat))
+
+    print(f"******************** {name} ********************")
+    print(f"Accuracy: {acc}")
+    print(f"Classification Report:\n {classreport}")
+    print(f"Confusion Matrix:\n {conf_mat}")
+    print(f"Coefficients: {coeffs.to_string()}")
+
+
+
+
+
+x = "Stop"

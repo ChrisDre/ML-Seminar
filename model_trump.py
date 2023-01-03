293391ec7e12dfabd91c631bb2f12a3ce1e0d46f
ChrisDre <drechselchris@gmail.com>
2023-01-03 14:43:40 +0100

    Implemented more algos. Commit before importing some variables with labels.

--- model_trump.py
+++ model_trump.py
@@ -1,26 +1,30 @@
 import pickle
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt 
 import numpy as np
 from pandas.api.types import CategoricalDtype
 import matplotlib.pyplot as plt
 from pandas.plotting import scatter_matrix
+import math
 
 
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler, OrdinalEncoder, OneHotEncoder
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score, precision_score, recall_score
-from sklearn import linear_model, tree
-from sklearn.decomposition import PCA
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.feature_selection import mutual_info_regression
-from sklearn.impute import SimpleImputer
-from sklearn.decomposition import FactorAnalysis
-from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn.ensemble import (
+    GradientBoostingClassifier,
+    RandomForestClassifier,
+    BaggingClassifier,
+    StackingClassifier
+)
 from sklearn.exceptions import NotFittedError
+from sklearn.feature_selection import SelectKBest, chi2, mutual_info_classif, f_classif
+
+
 
 
 from sklearn.linear_model import LogisticRegression, RidgeClassifierCV
@@ -40,16 +44,20 @@ from sklearn.metrics import confusion_matrix
 # load variable list from excel sheet
 df_vars = pd.read_excel("variables.xlsx", sheet_name="categories")
 
-# get chosen 'mental health' variables
+# get chosen variables
 chosen_vars = df_vars["Variable"].dropna().unique().tolist()
 
-# load gss data from 2018
+# load gss sruvey data from 2018 (already pre-filtered for easier import)
 df = pd.read_pickle("gss_2018_no_labels.pkl")
-# df = pd.read_pickle("gss_2018_labels.pkl")
+df_labels = pd.read_pickle("gss_2018_labels.pkl")
 
-# filter for mental vars
+# filter for trump vars
 df = df[chosen_vars]
+df_labels = df_labels[chosen_vars]
 
+# print first 5 rows and dimensions of unprocessed dataframe
+print(f"Unprocessed data - First rows: \n {df.head()}")
+print(f"Unprocessed data - Dimensions: {df.shape}")
 
 # load NAICs to classify industries
 ind_codes_xl = pd.read_excel("industry_codes.xlsx")
@@ -62,10 +70,11 @@ def preprocessing(raw_df):
     # reset index
     raw_df.reset_index(drop=True, inplace=True)
 
+    # id: -> drop "id" column
+    raw_df.drop("id", axis=1, inplace=True)
+
     # year: -> drop "year" column
     raw_df.drop("year", axis=1, inplace=True)
-
-    # id: -> do nothing
 
     # pres16: (Target Variable, 2016 presidential vote) -> remove rows with missing vote in 2016 (1 = Clinton, 2 = Trump, 3 = other candidate)
     raw_df = raw_df[raw_df.pres16.isin([1, 2, 3])].copy()
@@ -78,8 +87,12 @@ def preprocessing(raw_df):
     raw_df["wrkstat"] = raw_df["wrkstat"].astype("category")
 
     # hrs1: (Hours worked last week) -> replace missing values in hrs1 with hrs2 or zero for retired/unemp etc.
-    raw_df["hours_worked"] = raw_df["hrs1"].where(~raw_df["wrkstat"].isin([4, 5, 6, 7, 8]), 0)
-    raw_df["hours_worked"] = np.where(raw_df["wrkstat"] == 3, raw_df["hrs2"], raw_df["hours_worked"])
+    raw_df["hours_worked"] = raw_df["hrs1"].where(
+        ~raw_df["wrkstat"].isin([4, 5, 6, 7, 8]), 0
+    )
+    raw_df["hours_worked"] = np.where(
+        raw_df["wrkstat"] == 3, raw_df["hrs2"], raw_df["hours_worked"]
+    )
     raw_df.dropna(subset=["hours_worked"], inplace=True)
     raw_df.drop("hrs1", axis=1, inplace=True)
     raw_df.drop("hrs2", axis=1, inplace=True)
@@ -87,8 +100,7 @@ def preprocessing(raw_df):
     # wrkslf: (Self employed or not) -> if missing but retired/unemp etc. set to not self employed
     raw_df["selfemp"] = raw_df["wrkslf"].map({1: 1, 2: 0})
     raw_df["selfemp"] = raw_df["selfemp"].fillna(0)
-    raw_df["selfemp"] = raw_df["selfemp"].astype('int')
-
+    raw_df["selfemp"] = raw_df["selfemp"].astype("int")
 
     raw_df.drop("wrkslf", axis=1, inplace=True)
 
@@ -103,7 +115,6 @@ def preprocessing(raw_df):
 
     # relig: (Religion) -> nominal categorical
     raw_df["relig"] = raw_df["relig"].astype("category")
-
 
     # wrkgovt: (Gov employee or not) -> transform to binary govemp column
     raw_df["govemp"] = raw_df["wrkgovt"].map({1: 1, 2: 0})
@@ -151,9 +162,8 @@ def preprocessing(raw_df):
     raw_df.dropna(inplace=True)
 
     # convert some misspecified columns to int
-    raw_df["govemp"] = raw_df["govemp"].astype('int')
-    raw_df["fav_death_pen"] = raw_df["fav_death_pen"].astype('int')
-
+    raw_df["govemp"] = raw_df["govemp"].astype("int")
+    raw_df["fav_death_pen"] = raw_df["fav_death_pen"].astype("int")
 
     return raw_df
 
@@ -167,11 +177,54 @@ abt = preprocessing(abt)
 # show info
 abt.info()
 
-# TODO: visualization and summarization
-# TODO: feature selection
-# TODO: find classification algos
-# Models: Logitstic Regression, Naive Bayes Classifier, K-Nearest Neighbor, Decision Tree, Random Forest, Artificial Neural Networks, Support Vector Machines:
-# Evaluation: CV, Classification Report, ROC Curve, 
+# print first 5 rows and dimensions of processed dataframe
+print(f"Processed data - First rows: \n {abt.head()}")
+print(f"Processed data - Dimensions: {abt.shape}")
+
+# VISUALIZATION
+
+# categorical variables
+cat_vars = abt.select_dtypes(include=["category"]).columns
+
+cat_cols = 3
+cat_rows = math.ceil(len(cat_vars) / cat_cols)
+
+fig, axes = plt.subplots(cat_rows, cat_cols, figsize=(14,8))
+fig.suptitle("Categorical Variables", fontsize=12)
+
+for index, key in enumerate(cat_vars):
+    plt.subplot(cat_rows, cat_cols, index + 1)
+    sns.countplot(data=abt, x=key)
+fig.tight_layout()
+
+# binary vars
+bin_vars = abt.select_dtypes(include=["int"]).columns
+
+bin_cols = 3
+bin_rows = math.ceil(len(bin_vars) / bin_cols)
+
+fig, axes = plt.subplots(bin_rows, bin_cols, figsize=(14,8))
+fig.suptitle("Binary Variables", fontsize=12)
+
+for index, key in enumerate(bin_vars):
+    plt.subplot(bin_rows, bin_cols, index + 1)
+    sns.countplot(data=abt, x=key)
+fig.tight_layout()
+
+# continuous vars
+con_vars = abt.select_dtypes(include=["float"]).columns
+
+con_cols = 3
+con_rows = math.ceil(len(con_vars) / con_cols)
+
+fig, axes = plt.subplots(con_rows, con_cols, figsize=(14,8))
+fig.suptitle("Continuous Variables", fontsize=12)
+
+for index, key in enumerate(con_vars):
+    plt.subplot(con_rows, con_cols, index + 1)
+    sns.histplot(data=abt, x=key)
+fig.tight_layout()
+
 
 
 ## Models
@@ -180,7 +233,7 @@ abt.info()
 y = abt["trump"]
 
 # create feature colummns
-X = abt.drop(["trump", "id"], axis=1)
+X = abt.drop(["trump"], axis=1)
 
 # One Hot Encoding (create dummy vars for nominal categories), set drop_first=True to avoid dummy variable trap
 X = pd.get_dummies(X, drop_first=True)
@@ -190,7 +243,7 @@ X.drop("relig_8.0", axis=1, inplace=True)
 
 
 # TODO: Scale continuous variables
-cont_vars = abt.select_dtypes(include=['float']).columns
+cont_vars = abt.select_dtypes(include=["float"]).columns
 X[cont_vars] = StandardScaler().fit_transform(X[cont_vars])
 
 
@@ -200,169 +253,157 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 
+# feature selection
+# TODO: check k, output df, try different score functions
+def select_features(X_train, y_train, X_test):
+	fs = SelectKBest(score_func=mutual_info_classif, k=20)
+	fs.fit(X_train, y_train)
+	X_train_fs = fs.transform(X_train)
+	X_test_fs = fs.transform(X_test)
+	return X_train_fs, X_test_fs, fs
+
+
+X_train_fs, X_test_fs, fs = select_features(X_train, y_train, X_test)
+
+
+
 # set seed
 np.random.seed(5000)
 
 
 ## WITH HYPERPARAMETER TUNING
+# Bagging: RandomForest, Boosting: AdaBoost and Gradient Boost, Stacking: StackingClassifier Ensemble of different models
+# GaussianProcessClassifier, ADABoostClassifier,
+
+# TODO: what algos need standardized/normalized features
+
 # make pipelines
 pipelines = {
-    "LR": make_pipeline(LogisticRegression(solver="saga", max_iter=10000, random_state=1234)),
-    "LR_enet": make_pipeline(LogisticRegression(penalty="elasticnet", solver="saga", max_iter=10000, random_state=1234)),
+    "LR": make_pipeline(
+        LogisticRegression(penalty="none", max_iter=10000, random_state=1234)
+    ),
+    "LR_lasso": make_pipeline(
+        LogisticRegression(
+            penalty="l1", solver="saga", max_iter=10000, random_state=1234
+        )
+    ),
+    "LR_ridge": make_pipeline(
+        LogisticRegression(
+            penalty="l2", solver="saga", max_iter=10000, random_state=1234
+        )
+    ),
+    "LR_enet": make_pipeline(
+        LogisticRegression(
+            penalty="elasticnet", solver="saga", max_iter=10000, random_state=1234
+        )
+    ),
     "Ridge": make_pipeline(RidgeClassifierCV()),
     "DTC": make_pipeline(DecisionTreeClassifier(random_state=1234)),
     "RFC": make_pipeline(RandomForestClassifier()),
-    "KN": make_pipeline(KNeighborsClassifier()),
+    "KN": make_pipeline(MinMaxScaler(), KNeighborsClassifier()),
     "GNB": make_pipeline(GaussianNB()),
     "LDA": make_pipeline(LinearDiscriminantAnalysis()),
-    "SVC": make_pipeline(SVC(random_state=1234)),
-    "GB": make_pipeline(GradientBoostingClassifier(random_state=1234))
+    "SVC": make_pipeline(MinMaxScaler(), SVC(random_state=1234)),
+    "GB": make_pipeline(GradientBoostingClassifier(random_state=1234)),
+    "BDT": make_pipeline(BaggingClassifier(random_state=1234)),
+    "NN": make_pipeline(MLPClassifier(random_state=1234, max_iter=500)),
+    "Stack": make_pipeline(StackingClassifier(estimators=[
+        ('ridge', RidgeClassifierCV(alpha=0.1)),
+        ('lda', LinearDiscriminantAnalysis())
+        ], final_estimator=LogisticRegression(penalty="none", max_iter=10000, random_state=1234)))
 }
 
 # make hypergrid to find best parameters for each model
 hypergrid = {
-    "LR": {
-        "logisticregression__penalty": ["none", "l1", "l2"]
-        },
+    "LR": {},
+    "LR_lasso": {"logisticregression__C": np.logspace(-4, 4, 10)},
+    "LR_ridge": {"logisticregression__C": np.logspace(-4, 4, 10)},
     "LR_enet": {
-        "logisticregression__l1_ratio": [0.1, 0.5, 0.9]
-        },
-    "Ridge": {
-        "ridgeclassifiercv__alphas": [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 0.99]
-        },
+        "logisticregression__l1_ratio": np.arange(0, 1, 0.1),
+        "logisticregression__C": np.logspace(-4, 4, 10),
+    },
+    "Ridge": {"ridgeclassifiercv__alphas": np.arange(0.1, 1, 0.1)},
     "DTC": {
         "decisiontreeclassifier__min_samples_split": [2, 4, 6],
-        "decisiontreeclassifier__min_samples_leaf": [1, 2, 3]
-        },
+        "decisiontreeclassifier__min_samples_leaf": [1, 2, 3],
+    },
     "RFC": {
-        "randomforestclassifier__n_estimators": [50, 100, 200],
-        },
+        "randomforestclassifier__n_estimators": [10, 100, 1000],
+        "randomforestclassifier__max_features": ["sqrt", "log2"],
+    },
     "KN": {
         "kneighborsclassifier__leaf_size": [10, 20, 30, 40, 50],
-        "kneighborsclassifier__n_neighbors": [2, 3, 5, 7, 10],
+        "kneighborsclassifier__n_neighbors": np.arange(1, 20, 1),
         "kneighborsclassifier__p": [1, 2],
-        },
-    "GNB": {
-        "gaussiannb__var_smoothing": [1e-11, 1e-10, 1e-09]
-        },
-    "LDA": {
-        },
-    "SVC": {"svc__C": [0.1,1, 10, 100],
-            "svc__gamma": [1,0.1,0.01,0.001],
-            "svc__kernel": ['rbf', 'poly', 'sigmoid']
-        },
-    "GB": {"gradientboostingclassifier__n_estimators": [100, 200, 300]
-
-    }
+        "kneighborsclassifier__metric": ["euclidean", "manhattan", "minkowski"],
+        "kneighborsclassifier__weights": ["uniform", "distance"],
+    },
+    "GNB": {"gaussiannb__var_smoothing": [1e-11, 1e-10, 1e-09]},
+    "LDA": {},
+    "SVC": {
+        "svc__C": [0.1, 1, 10, 100],
+        "svc__gamma": [1, 0.1, 0.01, 0.001],
+        "svc__kernel": ["linear", "poly", "rbf", "sigmoid"],
+    },
+    "GB": {
+        "gradientboostingclassifier__n_estimators": [10, 100, 1000],
+        "gradientboostingclassifier__learning_rate": [0.001, 0.01, 0.1],
+    },
+    "BDT": {"baggingclassifier__n_estimators": [10, 100, 1000]},
+    "NN": {
+        "mlpclassifier__alpha": [0.0001, 0.05],
+        "mlpclassifier__learning_rate": ['constant','adaptive']
+    },
+    "Stack": {}
 }
 
 
-
-
+# Hyperparameter Tuning (find best parameters for each model)
 fit_models = {}
-
+print("Training models...")
 for algo, pipeline in pipelines.items():
     model = GridSearchCV(pipeline, hypergrid[algo], cv=10, n_jobs=-1)
-    print("Training models...")
     try:
         model.fit(X_train, y_train)
         fit_models[algo] = model
         print(f"{algo} successful...")
     except NotFittedError as e:
         print(repr(e))
-    print("Training models done.")
+print("Training models done.")
 
 
+# get best parameters
+# fit_models["LR_lasso"].best_params_
+
+# get all parameters with best parameters
+# fit_models["LR"].best_estimator_.get_params()
+
+        
 
 
 # evaluate performance on test partition
+results = {}
 for algo, model in fit_models.items():
     print(f"Predicting {algo}...")
+
     y_hat = model.predict(X_test)
+
+    classreport = classification_report(y_test, y_hat)
+    conf_mat = confusion_matrix(y_test, y_hat)
+
+
     print(f"Accuracy Score: {accuracy_score(y_test, y_hat)}")
     print(f"Precision Score: {precision_score(y_test, y_hat)}")
     print(f"Recall Score: {recall_score(y_test, y_hat)}")
-    
-
-
-
-
-
-
-
-## STANDARD MODEL WITHOUT HYPERPARAMETER TUNING
-# init list for comparison
-models = []
-
-
-def algo_models(X_train, X_test, y_train, y_test):
-
-    models.append(("LR", LogisticRegression(penalty='none', solver="saga", max_iter=10000)))
-    models.append(
-        ("LR_L1", LogisticRegression(penalty="l1", solver="saga", max_iter=10000))
-    )
-    models.append(
-        ("LR_L2", LogisticRegression(penalty="l2", solver="saga", max_iter=10000))
-    )
-    models.append(
-        (
-            "LR_EN",
-            LogisticRegression(
-                penalty="elasticnet", solver="saga", l1_ratio=0.5, max_iter=10000
-            ),
-        )
-    )
-    models.append(("Ridge", RidgeClassifierCV()))
-    models.append(("CART", DecisionTreeClassifier()))
-    models.append(("KNN", KNeighborsClassifier()))
-    models.append(("NB", GaussianNB()))
-    models.append(("LDA", LinearDiscriminantAnalysis()))
-    models.append(("SVM", SVC()))
-
-    outcomes = []
-    names = []
-
-    # loop through models
-    print(f"********** CV **********")
-    for name, model in models:
-        # 5-Fold Cross Validation (train, validate)
-        v_results = cross_val_score(
-            model, X_train, y_train, cv=5, scoring="accuracy", n_jobs=-1, verbose=0
-        )
-        print(f"5-CV Accuracy for {name}: {v_results.mean()}")
-        outcomes.append(v_results)
-        names.append(name)
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_xticklabels(names)
-    plt.boxplot(outcomes)
-
-
-algo_models(X_train, X_test, y_train, y_test)
-
-
-# Evaluating and predicting models
-for name, model in models:
-    trained_model = model.fit(X_train, y_train)
-
-    # prediction
-    y_pred = trained_model.predict(X_test)
-
-    acc = accuracy_score(y_test, y_pred)
-    classreport = classification_report(y_test, y_pred)
-    conf_mat = confusion_matrix(y_test, y_pred)
-    coeffs = pd.DataFrame(zip(X.columns, trained_model.coef_[0]))
-
-
-    print(f"******************** {name} ********************")
-    print(f"Accuracy: {acc}")
     print(f"Classification Report:\n {classreport}")
     print(f"Confusion Matrix:\n {conf_mat}")
-    print(f"Coefficients: {coeffs.to_string()}")
+
+    results[algo] = []
+    results[algo].append(accuracy_score(y_test, y_hat))
+    results[algo].append(precision_score(y_test, y_hat))
+    results[algo].append(recall_score(y_test, y_hat))
 
 
+results_df = pd.DataFrame.from_dict(results, orient='index', columns=["Accuracy", "Precision", "Recall"])
 
-
-
-x = "Stop"
+x = "stop"
